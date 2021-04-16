ee78360f3f0740eabd4a1a1b6be9754c6066b36c
kretzer <fabiolakretzer@gmail.com>
2021-04-15 21:53:03 -0300

    feat: add delete trainer pokemon

--- lib/api_exmon.ex
+++ lib/api_exmon.ex
@@ -10,4 +10,5 @@ defmodule ApiExmon do
   defdelegate fetch_pokemon(name_pokemon), to: Pokemon.Get, as: :call
 
   defdelegate create_trainer_pokemon(trainer_pokemons), to: TrainerPokemon.Create, as: :call
+  defdelegate delete_trainer_pokemon(id), to: TrainerPokemon.Delete, as: :call
 end
--- lib/api_exmon_web/controllers/trainer_pokemons_controller.ex
+++ lib/api_exmon_web/controllers/trainer_pokemons_controller.ex
@@ -9,6 +9,20 @@ defmodule ApiExmonWeb.TrainerPokemonsController do
     |> handle_response(conn, "create.json", :created)
   end
 
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ApiExmon.delete_trainer_pokemon()
+    |> handle_delete(conn)
+  end
+
+  defp handle_delete({:ok, _pokemon}, conn) do
+    conn
+    |> put_status(:no_content)
+    |>text("")
+  end
+
+  defp handle_delete({:error, _changeset} = error, _conn), do: error
+
   defp handle_response({:ok, pokemon}, conn, view, status) do
     conn
     |> put_status(status)
