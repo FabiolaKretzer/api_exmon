89adf059caee3d1fb021ee11c84269f0d35cab41
kretzer <fabiolakretzer@gmail.com>
2021-04-16 22:10:47 -0300

    test: add test trainers controller

---
+++ test/api_exmon_web/controllers/trainers_controller_test.exs
@@ -0,0 +1,29 @@
+defmodule ApiExmonWeb.TrainersControllerTest do
+ use ApiExmonWeb.ConnCase
+
+ alias ApiExmon.Trainer
+
+ describe "show/2" do
+  test "when there is a trainer with the given id, returns the trainer", %{conn: conn} do
+    params = %{name: "Name 1", password: "123456"}
+
+    {:ok, %Trainer{id: id}} = ApiExmon.create_trainer(params)
+
+    response =
+      conn
+      |> get(Routes.trainers_path(conn, :show, id))
+      |> json_response(:ok)
+
+    assert %{"id" => _id, "inserted_at" => _inserted_at, "name" => "Name 1"} = response
+  end
+
+  test "when there is an error, returns the error", %{conn: conn} do
+    response =
+      conn
+      |> get(Routes.trainers_path(conn, :show, "1234"))
+      |> json_response(:bad_request)
+
+    assert response == %{"message" => "Invalid ID format!"}
+  end
+ end
+end
